6ce9d47d2a7491a3e2fec5c8917e211cfdeae4d8
alucard3181992 <cristianmiranda41@.com>
2023-11-27 17:32:25 -0400

    Start

--- src/lib/prisma.jsx
+++ src/lib/prisma.jsx
@@ -14,7 +14,8 @@ if (process.env.NODE_ENV === 'production') {
 export default prisma; */
 
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated/client"
+//const prisma = new PrismaClient()
 
 const prisma = new PrismaClient({
     log: ["info"]
