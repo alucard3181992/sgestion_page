78271615b5fffa959ea59733c0db87d136da62c7
alucard3181992 <cristianmiranda41@.com>
2024-08-17 16:03:41 -0400

    Start

--- src/pages/api/excel/generadorExcel.jsx
+++ src/pages/api/excel/generadorExcel.jsx
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
       try {
         // Cargar el archivo Excel
         console.log("SOY EL PINCHE GET DE EXCEL");
-        const workbook = await XlsxPopulate.fromFileAsync("./public/icons/h/out.xlsx");
+        const workbook = await XlsxPopulate.fromFileAsync("/icons/h/out.xlsx");
 
         // Seleccionar la hoja que contiene la tabla
         const sheet = workbook.sheet(0);
